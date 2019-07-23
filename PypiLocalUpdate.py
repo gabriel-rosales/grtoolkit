6de48d3f03bcd3f8120d9f38f7510ad9350b751f
unknown <gabriel.alejandro.rosales@gmail.com>
2019-07-22 23:40:17 -0400

    Update

---
+++ PypiLocalUpdate.py
@@ -0,0 +1,6 @@
+packageName = "grtoolkit"
+#UPDATE LOCATE PACKAGE TO LATEST VER ON PYPI
+from grtoolkit.Windows import cmd
+install_package = f'pip install --upgrade {packageName} --user'
+cmd(install_package,
+    install_package)
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def filesInFolder(folder, fileType):  # Returns list of files of specified file 
 
 setuptools.setup(
     name="grtoolkit",
-    version="19.07.28",
+    version="19.07.30",
     author="Gabriel Rosales",
     author_email="gabriel.alejandro.rosales@gmail.com",
     description="Common functions for quick python development",

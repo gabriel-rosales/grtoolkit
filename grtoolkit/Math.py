c58e20fff181bb5487b24cd13830bece53cd7424
Gabriel Rosales <GabrielR@garrtech.local>
2019-08-07 17:24:41 -0400

    Update

--- grtoolkit/Math.py
+++ grtoolkit/Math.py
@@ -1,6 +1,13 @@
-import math
+import math, numpy as np
 
 def roundSig(x, sig=5):
     if x == 0:
         return 0
     return round(x, sig - int(math.floor(math.log10(abs(x)))) - 1)
+
+def softmax(L):
+    '''Function that takes as input a list of numbers, and returns the list of values given by the softmax function.'''
+    return [np.exp(x)/sum(np.exp(L)) for x in L]
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))

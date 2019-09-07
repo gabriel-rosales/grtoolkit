6c2f0413540da65d9e95d020673c6d9b7793e785
unknown <gabriel.alejandro.rosales@gmail.com>
2019-09-07 17:57:44 -0400

    Update

--- grtoolkit/Math.py
+++ grtoolkit/Math.py
@@ -19,12 +19,26 @@ def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 def cross_entropy(Y, P):
-    '''Sample inputs: Y=[1,0,1,1] and P=[0.4,0.6,0.1,0.5]
+    '''Sample inputs: Y=[1,1,0] and P=[0.8,0.7,0.1] Equal 0.69
     Where,
     Y is list of whether events are on or off
     P is list of probabilities if events are on
     
-    Formula is sum of all cases of entropy if event is active plus entropy if event is inactive'''
+    Formula is sum of all cases of entropy if event is active plus entropy if event is inactive
+    
+    Events with high probabilities have low cross entopy
+    Events with low proabilities have high cross entropy
+
+    OR
+
+    High cross entropy means low probability
+    Low cross entropy means high probability
+    '''
+   
     Y = np.float_(Y)
     P = np.float_(P)
     return -np.sum(Y * np.log(P) + (1 - Y) * np.log(1 - P))
+
+w1 = 3; w2=5; b=-2.2
+x = sigmoid(w1*.4 + w2*.6 + b)
+print(x)

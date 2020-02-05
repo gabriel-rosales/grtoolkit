76e984fb018ef75ed8e59b318ba28e26bf09146d
unknown <gabriel.alejandro.rosales@gmail.com>
2020-02-04 21:58:11 -0500

    Update

--- grtoolkit/Circuits/__init__.py
+++ grtoolkit/Circuits/__init__.py
@@ -29,3 +29,27 @@ def Resistance(find, printEq=False, **kwargs):
     eq = list()
     eq.append("Eq(R,p*l/A)")    
     return solveEqs(eq, find, printEq=printEq, **kwargs)
+
+def KCL(i_list):
+    """Sum of currents entering a node equal the sum of currents leaving a node"""
+    return sum(i_list)
+
+def KVL(v_list):
+    """Sum of all voltages around a closed path (or loop) is 0"""
+    return sum(v_list)
+
+def ResistorsInSeries(r_list):
+    """Resistors connected in series is the sum of the individual resistances"""
+    return sum(r_list)
+
+def ResistorsInParellel(r_list):
+    sumOfInverse = sum([1/r for r in r_list])
+    return 1/sumOfInverse
+
+def voltageDivision(v_in, r_list_ordered, showWork=False):
+    """voltage is divided among the resistors in direct proportion to their resistances; the larger the resistance, the larger the voltage drop."""
+    r_total = sum(r_list_ordered)
+    voltages = [r/r_total*v_in for r in r_list_ordered]
+    print("Resistor ordered voltage division: ", voltages)
+    return voltages
+

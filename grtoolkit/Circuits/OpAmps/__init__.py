6d5615d8f290e563130150079f1f9b5935217ac4
Gabriel Rosales <GabrielR@garrtech.local>
2020-02-05 11:04:55 -0500

    Update

---
+++ grtoolkit/Circuits/OpAmps/__init__.py
@@ -0,0 +1,51 @@
+# An op amp is an active circuit element designed to perform mathematical operations of addition, subtraction, multiplication, division, dif￾ferentiation, and integration.
+
+from grtoolkit.Math import solveEqs
+
+def referenceMaterial():
+    #Open reference material
+    pass
+
+def invertingAmplifier(find="v_out", printEq=False, **kwargs):
+    """variables: 
+                v_out, v_in = voltage out/in
+                R2, R1 = resistors (view reference image)
+                """
+    eq = list()
+    eq.append("Eq(v_out,-R2/R1*v_in)")
+    return solveEqs(eq, find, printEq=printEq, **kwargs)
+
+def nonInvertingAmplifier(find="v_out", printEq=False, **kwargs):
+    """variables: 
+                v_out, v_in = voltage out/in
+                R2, R1 = resistors (view reference image)
+                """
+    eq = list()
+    eq.append("Eq(v_out,(1+R2/R1)*v_in)")
+    return solveEqs(eq, find, printEq=printEq, **kwargs)
+
+def voltageFollower(find="v_out", printEq=False, **kwargs):
+    """variables: 
+                v_out, v_in = voltage out/in (view reference image)
+                """
+    eq = list()
+    eq.append("Eq(v_out,v_in)")
+    return solveEqs(eq, find, printEq=printEq, **kwargs)
+
+def summer(find="v_out", printEq=False, **kwargs):
+    """variables: 
+                v_out, v1, v2, v3 = voltage out/in 
+                Rf, R3, R2, R1 = resistors (view reference image)
+                """
+    eq = list()
+    eq.append("Eq(v_out,-(Rf/R1*v1 + Rf/R2*v2 + Rf/R3*v3))")
+    return solveEqs(eq, find, printEq=printEq, **kwargs)
+
+def differenceAmplifier(find="v_out", printEq=False, **kwargs):
+    """variables: 
+                v_out, v1, v2, v3 = voltage out/in 
+                R2, R1 = resistors (view reference image)
+                """
+    eq = list()
+    eq.append("Eq(v_out,R2/R1*(v2-v1)")
+    return solveEqs(eq, find, printEq=printEq, **kwargs)
--- grtoolkit/Circuits/__init__.py
+++ grtoolkit/Circuits/__init__.py
@@ -1,4 +1,5 @@
 import grtoolkit.Circuits.Filters
+import grtoolkit.Circuits.OpAmps
 import grtoolkit.Circuits.Transistors
 
 from grtoolkit.Math import solveEqs
@@ -30,6 +31,16 @@ def Resistance(find, printEq=False, **kwargs):
                 A=cross sectional area"""
     eq = list()
     eq.append("Eq(R,p*l/A)")    
+    return solveEqs(eq, find, printEq=printEq, **kwargs)
+
+def Capacitance(find="C", printEq=False, **kwargs):
+    """variables: 
+                C = capacitance
+                epsilon = permitivity of the dielectric material between plates
+                A = surface area of each plate
+                d = distance between plates"""
+    eq = list()
+    eq.append("Eq(C,epsilon*A/d)")    
     return solveEqs(eq, find, printEq=printEq, **kwargs)
 
 def KCL(i_list):
@@ -113,4 +124,14 @@ def meshAnalysis():
     print("""
         Sum of all voltages in a node""")
 
+def sourceTransformation():
+    # TO BE REVIEWED
+    pass
 
+def TheveninNorton():
+    # TO BE REVIEWED
+    pass
+
+def maximumPowerTransfer():
+    # TO BE REVIEWED
+    pass

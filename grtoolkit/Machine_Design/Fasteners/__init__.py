f6706f80c27c28b32e75bd750d10bb9ca6f01773
Gabriel Rosales <GabrielR@garrtech.local>
2020-11-14 00:51:48 -0500

    update

--- grtoolkit/Machine_Design/Fasteners/__init__.py
+++ grtoolkit/Machine_Design/Fasteners/__init__.py
@@ -1,5 +1,22 @@
+from sympy import pi
+
 def screwThreadEngagement(diameter, screw="steel", substrate="aluminium"):
     if substrate == "aluminium":
         return 2*diameter
     if substrate == "steel" or substrate == "iron" or substrate == "brass" or substrate == "bronze":
         return 2*diameter
+
+def A_t(find="A_t", printEqs=True, **kwargs):
+    """
+    Tensile Stress Area
+
+    Where:
+        A_t = Tensile Stress Area
+        dp, dr = pitch diameter, root diameter
+    """
+    eq = list()
+    eq.append("Eq(A_t, pi/4*((d_p + d_r)/2)**2)") #Approximation
+    return solveEqs(eq, find=find, printEq=printEqs, **kwargs)
+
+
+
